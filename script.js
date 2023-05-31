f7822e9a2803bf1f82f8e32e68d7a6f648ef7bf5
Elizabeth Ahumuza <elizabeth@PNPs-MacBook-Pro.local>
2023-05-31 23:52:59 +0300

    modified script.js

--- script.js
+++ script.js
@@ -1,51 +1,55 @@
-       const inputCity = document.getElementById('location')
-       const form = document.querySelector('form')
-       const weatherInfo = document.getElementById('weatherInfo')
-      
-        async function getWeatherInfo(city){
-            try{
-                const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=ac363587152241107a97b9599922302a`
-                const response = await fetch (url)
-                const data = await response. json()
-                console.log (data)
-                const weatherDescription = data.weather[0].description;
-                const temperature = data.main.temp;
-                const div1 = document.createElement('div')
-                div1.innerText= `${weatherDescription}`
-                div1.classList.add('desc')
-                
-                const div2 = document.createElement('div')
-                div2.innerText= `${city}`
-                div2.classList.add('cit')
-                
-                const div3 = document.createElement('div')
-                div3.innerText= `${temperature} \u00B0C`
-                div3.classList.add('deg')
-              
-                weatherInfo.appendChild(div1)
-                weatherInfo.appendChild(div2)
-                weatherInfo.appendChild(div3)
+const inputCity = document.getElementById("location");
+const form = document.querySelector("form");
+const weatherInfo = document.getElementById("weatherInfo");
+const errorElement = document.getElementById("error");
 
-            }
-            
-            catch(error){
-                const err = document.getElementById('error')
-                err.textContent= 'error fetching weather data'
-            }    
-            
-        }
-       
-       form.addEventListener('submit',  async(event) => {
-          event.preventDefault();
+async function fetchWeatherData(city) {
+  try {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=ac363587152241107a97b9599922302a`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    throw new Error("Error fetching weather data");
+  }
+}
 
-          const city = inputCity.value;
-          await getWeatherInfo(city); 
-        });
-     
-    
+function renderWeatherInfo(weatherData) {
+  const weatherDescription = weatherData.weather[0].description;
+  const temperature = weatherData.main.temp;
 
+  const weatherDescDiv = document.createElement("div");
+  weatherDescDiv.innerText = `${weatherDescription}`;
+  weatherDescDiv.classList.add("desc");
 
-       
+  const cityDiv = document.createElement("div");
+  cityDiv.innerText = inputCity.value;
+  cityDiv.classList.add("cit");
 
+  const temperatureDiv = document.createElement("div");
+  temperatureDiv.innerText = `${temperature} \u00B0C`;
+  temperatureDiv.classList.add("deg");
 
-     
+  weatherInfo.innerHTML = "";
+  weatherInfo.appendChild(weatherDescDiv);
+  weatherInfo.appendChild(cityDiv);
+  weatherInfo.appendChild(temperatureDiv);
+
+  errorElement.textContent = "";
+}
+
+async function getWeatherInfo(city) {
+  try {
+    const weatherData = await fetchWeatherData(city);
+    renderWeatherInfo(weatherData);
+  } catch (error) {
+    errorElement.textContent = error.message;
+  }
+}
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  const city = inputCity.value;
+  await getWeatherInfo(city);
+});
